8b8ef713767fdfc5f196a7efb1c07fe7cdc46671
Tanvir <tanvir.info07@gmail.com>
2024-08-29 08:41:07 -0700

    dashboard app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="bg-red-400 flex items-center justify-center text-3xl h-screen w-screen">
+        hello world
+      </p>
+    </>
+  );
+}
+
+export default App;
